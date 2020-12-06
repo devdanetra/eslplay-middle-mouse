731d2dfb8ea631fdb5fbe8b41a933c8fdc8a70f3
Francesco D'anetra <devdanetra@outlook.com>
2020-12-06 21:17:33 +0100

    added User object, and factory constructor
    -added user object
    -added getFromID  factory constructor
    -defined loginData object

--- middlemouse.js
+++ middlemouse.js
@@ -25,13 +25,13 @@ class MiddleMouse {
                     return pathArray[j]; //getting only ID numbers
                 }
             }
-        }else return MiddleMouseUtils.CANT_REACH_PLAYERID;
+        } else return MiddleMouseUtils.CANT_REACH_PLAYERID;
     }
 
-    static get teamID(){
+    static get teamID() {
         var path = window.location.pathname; //getting current path
-        var activationPaths = MiddleMouseUtils.TEAMMEMBERSIDS_ACTIVATION_PATH_REGEX.exec(path); //getting list of paths where playerID can be reached and scraped from the current window path.
-        if (activationPaths != null) { //checking if we can actually find the playerID
+        var activationPaths = MiddleMouseUtils.TEAMID_ACTIVATION_PATH_REGEX.exec(path); //getting list of paths where teamID can be reached and scraped from the current window path.
+        if (activationPaths != null) { //checking if we can actually find the teamID
             var pathArray = path.split("/");
             var mainPath = activationPaths[0].replaceAll("/", ""); //getting main path 
             for (var j = pathArray.indexOf(mainPath); j < pathArray.length; j++) {
@@ -40,7 +40,11 @@ class MiddleMouse {
                     return pathArray[j]; //getting only ID numbers
                 }
             }
-        }else return MiddleMouseUtils.CANT_REACH_TEAMID;
+        } else return MiddleMouseUtils.CANT_REACH_TEAMID;
+    }
+
+    static get teamMembers() {
+        //todo make this working
     }
 
 }
@@ -49,9 +53,69 @@ class MiddleMouse {
 class MiddleMouseUtils {
     static ID_REGEX = new RegExp('^[0-9]+$');
     static PLAYERID_ACTIVATION_PATH_REGEX = new RegExp('\/(player|admin_leaguebans|admin_exceptions|admin_leaguepenalties|admin_barrages|contestant)\/');
-    static TEAMMEMBERSIDS_ACTIVATION_PATH_REGEX = new RegExp('\/(team|admin_leaguepenalties|admin_exceptions|team_permissions|contestant|)\/');
+    static TEAMID_ACTIVATION_PATH_REGEX = new RegExp('\/(team|admin_leaguepenalties|admin_exceptions|team_permissions|contestant|)\/');
 
     static CANT_REACH_PLAYERID = "Can't reach player ID. You can't use this command here";
     static CANT_REACH_TEAMID = "Can't reach team ID. You can't use this command here";
 
+}
+
+class LoginData{
+    constructor(ip,date,service){
+        this.ip=ip;
+        this.date = date;
+        this.service = service;
+    };
+}
+
+class User {
+    
+    constructor(id, name, nick, registerDate, age, gender, nationality, country, mainTeam,homepage) {
+        this.id = id;
+        this.name = name;
+        this.nick = nick;
+        this.registerDate = registerDate;
+        this.age = age;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.country = country;
+        this.mainTeam = mainTeam;
+        this.homepage = homepage;
+    }
+
+    getAllLoginData(){
+        //todo make this working
+    }
+
+    static async getFromID(id) { //todo add error handling
+        var name,nick,registerDate,age,gender,nationality,country,mainTeam,homepage;
+        await axios.get(`https://play.eslgaming.com/player/${id}/`).then((response) => { //getting user page from id
+            if (response.status == 200) {
+
+                var page = $.parseHTML(response.data);
+                var elements = $('.playerprofile_stammdaten', page).children().children().children().next(); //Getting profile data
+
+                var results = new Array();
+                Array.from(elements).forEach((e) => {
+                    results.push(e.innerText.trim());
+                });
+
+                //PARSING RESULTS
+                name = results[0];
+                nick = results[1];
+                registerDate = results[2];
+                age = results[3].split("/")[0].trim();
+                gender = results[3].split("/")[1].trim();
+                nationality = results[4];
+                country = results[5];
+                mainTeam = results[6];
+                homepage = results[7];
+
+                console.log(name + " | " + nick + " | " + registerDate + " | " + age + " | " + gender + " | " + nationality + " | " + country);
+
+            } else return console.log("unable to fetch player id " + id);
+        });
+        return new User(id,name, nick, registerDate, age, gender, nationality, country,mainTeam,homepage);
+
+    }
 }
